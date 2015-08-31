0cbda638007ea76eb005e09e59eaf5b4c45aefef
mpjr1 <mapg.jr@gmail.com>
2015-08-31 14:46:37 +0000

    api controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,12 @@ Rails.application.routes.draw do
     resources :events, except: [:index, :show]
   end
   
-  root to: 'welcome#index'
+   namespace :api, defaults: { format: :json } do
+     match '/events', to: 'events#index', via: [:options]
+     resources :events, only: [:create]
+   end
+  
+  root to: 'registered_applications#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
